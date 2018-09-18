edb9cfdf9b3b15ec8a7ffb09c1cf1eea95a9e7b7
NataliiaMk <n.zahryvna47@gmail.com>
2018-09-18 13:11:32 +0300

    Document name has been changed.  Added information about steps of Gmail Sing in

--- Google_Gmail.py
+++ Google_Gmail.py
@@ -9,11 +9,20 @@ driver = webdriver.Chrome('C:\Chrome_webdriver\chromedriver.exe')
 
 driver.get ('https://www.google.com/')
 assert "Google" in driver.title
+print('Google page has been opened and checked')
+
 element1 = driver.find_element_by_link_text('Gmail').click()
+print('Gmail tab has been opened')
 element2 = driver.find_element_by_class_name('gmail-nav__nav-link__sign-in').click()
+print('Sign in button has been pressed')
 element3 = driver.find_element_by_id('identifierId').send_keys('SupervisorTestITA@gmail.com' + Keys.ENTER)
+print('user id has been entered')
 
 time.sleep(5)
 passwordField = driver.find_element_by_class_name('zHQkBf').send_keys('qwerty123456q' + Keys.ENTER)
+print('user password has been entered')
 
-time.sleep(3)
+time.sleep(5)
+
+driver.quit()
+print('Chrome browser has been closed')
