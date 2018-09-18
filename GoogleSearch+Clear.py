31d73a0c55e4c494bc2c6c8fe81888cbab6dc38e
NataliiaMk <n.zahryvna47@gmail.com>
2018-09-18 13:20:02 +0300

    Added console output for clearing search line

--- GoogleSearch+Clear.py
+++ GoogleSearch+Clear.py
@@ -8,12 +8,20 @@ driver = webdriver.Chrome('C:\Chrome_webdriver\chromedriver.exe')
 
 driver.get ('https://www.google.com/')
 assert "Google" in driver.title
+print('Google page has been opened and checked')
 search1 = driver.find_element_by_name('q')
 search1.send_keys('Python' + Keys.ENTER)
+print('Search has been performed')
+
 time.sleep(3)
 search2 = driver.find_element_by_id('lst-ib').click()
 search2 = driver.find_element_by_id('lst-ib').clear()
-time.sleep(2)
+print('Data has been cleared')
+
+time.sleep(5)
 driver.refresh()
+print('Page has been refreshed')
+
 time.sleep(3)
 driver.quit()
+print("Test successfully finished")
